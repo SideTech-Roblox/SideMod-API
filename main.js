cd1231cbe879aa3b606f96e0ac0051f3189f0740
DaRealGandhi20 <81023335+DaRealGandhi20@users.noreply.github.com>
2023-05-23 17:43:54 -0400

    Schedule API Test

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ const requireHeader = (headerName) => {
     if (req.headers[headerName]) {
       next();
     } else {
-      res.status(400).send(`Missing required header: ${headerName}`);
+      res.status(400).json({ error: `Missing required header: ${headerName}` });
     }
   };
 };
@@ -70,55 +70,55 @@ app.get("/notes/get", requireHeader('guild-id'), requireHeader('api-token'), (re
   const Token = req.headers['api-token'];
 
   const UserId = req.query.userid;
-  
+
   if (UserId) {
-     get(child(DatabaseDownload, `GuildsDatabase/${GuildId}`)).then((snapshot) => {
-       if (snapshot.exists()) {
-         get(child(DatabaseDownload, `APIKeyDatabase/${GuildId}`)).then((snapshot2) => {
-           if (snapshot2.exists()) {
-             if (snapshot2.val() === Token) {
-               if (snapshot.child('UserData').exists()) {
-                 if (snapshot.child('UserData').child(UserId).exists()) {
-                    if (snapshot.child('UserData').child(UserId).child('Notes').exists()) {
-                      const data = {
-                        RobloxId: UserId,
-                        Notes: snapshot.child('UserData').child(UserId).child('Notes').val()
-                      };
-                      res.status(200).json(data);
-                    } else {
-                      const data = {
-                        RobloxId: UserId,
-                        Notes: 'None'
-                      };
-                      res.status(200).json(data);
+    get(child(DatabaseDownload, `GuildsDatabase/${GuildId}`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        get(child(DatabaseDownload, `APIKeyDatabase/${GuildId}`)).then((snapshot2) => {
+          if (snapshot2.exists()) {
+            if (snapshot2.val() === Token) {
+              if (snapshot.child('UserData').exists()) {
+                if (snapshot.child('UserData').child(UserId).exists()) {
+                  if (snapshot.child('UserData').child(UserId).child('Notes').exists()) {
+                    const data = {
+                      RobloxId: UserId,
+                      Notes: snapshot.child('UserData').child(UserId).child('Notes').val()
                     };
-                 } else {
-                   const data = {
-                     RobloxId: UserId,
-                     Notes: 'None'
-                   };
-                   res.status(200).json(data);
-                 };
-               } else {
-                 const data = {
-                   RobloxId: UserId,
-                   Notes: 'None'
-                 };
-                 res.status(200).json(data);
-               };
-             } else {
-               res.status(400).json({ error: "Invalid API Token!" });
-             }; 
-           } else {
-             res.status(400).json({ error: "Invalid API Token!" });
-           };
-         });
-       } else {
-          res.status(400).json({ error: "Invalid GuildId!" });
-       };
-     });
+                    res.status(200).json(data);
+                  } else {
+                    const data = {
+                      RobloxId: UserId,
+                      Notes: 'None'
+                    };
+                    res.status(200).json(data);
+                  };
+                } else {
+                  const data = {
+                    RobloxId: UserId,
+                    Notes: 'None'
+                  };
+                  res.status(200).json(data);
+                };
+              } else {
+                const data = {
+                  RobloxId: UserId,
+                  Notes: 'None'
+                };
+                res.status(200).json(data);
+              };
+            } else {
+              res.status(400).json({ error: "Invalid API Token!" });
+            };
+          } else {
+            res.status(400).json({ error: "Invalid API Token!" });
+          };
+        });
+      } else {
+        res.status(400).json({ error: "Invalid GuildId!" });
+      };
+    });
   } else {
-   res.status(400).json({ error: "Invalid User!" });
+    res.status(400).json({ error: "Invalid User!" });
   }
 });
 
@@ -127,100 +127,120 @@ app.get("/points/get", requireHeader('guild-id'), requireHeader('api-token'), (r
   const Token = req.headers['api-token'];
 
   const UserId = req.query.userid;
-  
+
   if (UserId) {
-     get(child(DatabaseDownload, `GuildsDatabase/${GuildId}`)).then((snapshot) => {
-       if (snapshot.exists()) {
-         get(child(DatabaseDownload, `APIKeyDatabase/${GuildId}`)).then((snapshot2) => {
-           if (snapshot2.exists()) {
-             if (snapshot2.val() === Token) {
-               if (snapshot.child('UserData').exists()) {
-                 if (snapshot.child('UserData').child(UserId).exists()) {
-                    if (snapshot.child('UserData').child(UserId).child('Points').exists()) {
-                      const data = {
-                        RobloxId: UserId,
-                        Points: snapshot.child('UserData').child(UserId).child('Points').val()
-                      };
-                      res.status(200).json(data);
-                    } else {
-                      const data = {
-                        RobloxId: UserId,
-                        Points: 0
-                      };
-                      res.status(200).json(data);
+    get(child(DatabaseDownload, `GuildsDatabase/${GuildId}`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        get(child(DatabaseDownload, `APIKeyDatabase/${GuildId}`)).then((snapshot2) => {
+          if (snapshot2.exists()) {
+            if (snapshot2.val() === Token) {
+              if (snapshot.child('UserData').exists()) {
+                if (snapshot.child('UserData').child(UserId).exists()) {
+                  if (snapshot.child('UserData').child(UserId).child('Points').exists()) {
+                    const data = {
+                      RobloxId: UserId,
+                      Points: snapshot.child('UserData').child(UserId).child('Points').val()
                     };
-                 } else {
-                   const data = {
-                     RobloxId: UserId,
-                     Points: 0
-                   };
-                   res.status(200).json(data);
-                 };
-               } else {
-                 const data = {
-                   RobloxId: UserId,
-                   Points: 0
-                 };
-                 res.status(200).json(data);
-               };
-             } else {
-               res.status(400).json({ error: "Invalid API Token!" });
-             }; 
-           } else {
-             res.status(400).json({ error: "Invalid API Token!" });
-           };
-         });
-       } else {
-          res.status(400).json({ error: "Invalid GuildId!" });
-       };
-     });
+                    res.status(200).json(data);
+                  } else {
+                    const data = {
+                      RobloxId: UserId,
+                      Points: 0
+                    };
+                    res.status(200).json(data);
+                  };
+                } else {
+                  const data = {
+                    RobloxId: UserId,
+                    Points: 0
+                  };
+                  res.status(200).json(data);
+                };
+              } else {
+                const data = {
+                  RobloxId: UserId,
+                  Points: 0
+                };
+                res.status(200).json(data);
+              };
+            } else {
+              res.status(400).json({ error: "Invalid API Token!" });
+            };
+          } else {
+            res.status(400).json({ error: "Invalid API Token!" });
+          };
+        });
+      } else {
+        res.status(400).json({ error: "Invalid GuildId!" });
+      };
+    });
   } else {
-   res.status(400).json({ error: "Invalid User!" });
+    res.status(400).json({ error: "Invalid User!" });
   }
 });
 
-// app.get("/schedule/get", requireHeader('guild-id'), requireHeader('api-token'), (req, res) => {
-//   const GuildId = req.headers['guild-id'];
-//   const Token = req.headers['api-token'];
+app.get("/schedule/get", requireHeader('guild-id'), requireHeader('api-token'), (req, res) => {
+  const GuildId = req.headers['guild-id'];
+  const Token = req.headers['api-token'];
 
-//      get(child(DatabaseDownload, `GuildsDatabase/${GuildId}`)).then((snapshot) => {
-//        if (snapshot.exists()) {
-//          get(child(DatabaseDownload, `APIKeyDatabase/${GuildId}`)).then((snapshot2) => {
-//            if (snapshot2.exists()) {
-//              if (snapshot2.val() === Token) {
-//                get(child(DatabaseDownload, `GuildsDatabase/${GuildId}/Schedule`)).then((snapshot3) => {
-// 								if (snapshot3.exists()) {
-//                   let currenttime = Math.floor(Date.now() / 1000);
+  get(child(DatabaseDownload, `GuildsDatabase/${GuildId}`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      get(child(DatabaseDownload, `APIKeyDatabase/${GuildId}`)).then((snapshot2) => {
+        if (snapshot2.exists()) {
+          if (snapshot2.val() === Token) {
+            get(child(DatabaseDownload, `GuildsDatabase/${GuildId}/Schedule`)).then((snapshot3) => {
+              if (snapshot3.exists()) {
+                let currenttime = Math.floor(Date.now() / 1000);
 
-//                   const sortedSnapshots = [];
-									
-// 									snapshot3.forEach(function (childSnapshot3) {
-// 									  if (currenttime >= childSnapshot3.child('timestamp').val()) {
-//                     } else {
-// 										sortedSnapshots.push(childSnapshot3);
-// 									  }
-// 									});
-                  
-//                 } else {
-//                    res.status(400).json({ error: "There are currently 0 events scheduled!" });
-// 								};
-// 							});
-//              } else {
-//                res.status(400).json({ error: "Invalid API Token!" });
-//              }; 
-//            } else {
-//              res.status(400).json({ error: "Invalid API Token!" });
-//            };
-//          });
-//        } else {
-//           res.status(400).json({ error: "Invalid GuildId!" });
-//        };
-//      });
-// });
+                const sortedSnapshots = [];
+
+                snapshot3.forEach(function (childSnapshot3) {
+                  if (currenttime <= childSnapshot3.child('timestamp').val()) {
+                    sortedSnapshots.push(childSnapshot3);
+                  }
+                });
+
+                if (sortedSnapshots.length === 0) {
+                  return res.status(400).json({ error: "There are currently 0 events scheduled!" });
+                }
+
+                sortedSnapshots.sort(function (a, b) {
+                  return a.child('timestamp').val() - b.child('timestamp').val();
+                });
+
+                const list = {};
+
+                sortedSnapshots.forEach(function (snapshot4) {
+                  list[snapshot4.key] = {
+                    host: snapshot4.child('host').val(),
+                    duration: snapshot4.child('duration').val(),
+                    notes: snapshot4.child('notes').val(),
+                    eventtype: snapshot4.child('eventtype').val(),
+                    timestamp: snapshot4.child('timestamp').val(),
+                  };
+                });
+
+                res.status(200).json(list);
+              } else {
+                res.status(400).json({ error: "There are currently 0 events scheduled!" });
+              };
+            });
+          } else {
+            res.status(400).json({ error: "Invalid API Token!" });
+          };
+        } else {
+          res.status(400).json({ error: "Invalid API Token!" });
+        };
+      });
+    } else {
+      res.status(400).json({ error: "Invalid GuildId!" });
+    };
+  });
+});
 
 app.get("/verify/get", (req, res) => {
   const userID = req.query.userid;
-  
+
   if (userID) {
     get(child(DatabaseDownload, `VerifyDatabase/${userID}`)).then((snapshot) => {
       if (snapshot.exists()) {
